eea47f063e85049270055ed1675470ea2b469154
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-11 19:46:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function abrirAltaArticulo() {
 
     // Oculto todos los formularios menos este
     $("form:not('#frmAltaArticulo')").hide("normal");
+    $("#body").hide("normal");
 
     // Verifico si ya he cargado el formulario antes
     if ($('#frmAltaArticulo').size() == 0) {
