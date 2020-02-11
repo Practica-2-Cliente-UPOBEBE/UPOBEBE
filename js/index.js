f02fb54e173ba9c59c2d1bb7cfdd0eda12351012
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-11 19:33:46 +0100

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Carga dinámica de formularios
+$("#altaArticulo").click(abrirAltaArticulo);
+
+
+function abrirAltaArticulo() {
+
+    // Oculto todos los formularios menos este
+    $("form:not('#frmAltaArticulo')").hide("normal");
+
+    // Verifico si ya he cargado el formulario antes
+    if ($('#frmAltaArticulo').size() == 0) {
+        $("<div>").appendTo('#formularios').load("altaArticulo/altaArticulo.html",
+            function() {
+                $.getScript("altaArticulo/altaArticulo.js");
+            });
+
+    } else {
+        // Lo muestro si está oculto
+        $('#frmAltaArticulo').show("normal");
+    }
+}
