61e9ddda8951008bdc30362ebd371fb0495546d0
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-18 15:57:36 +0100

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -18,7 +18,7 @@ document.getElementById("mostrarPaginaPrincipal").addEventListener("click",fMost
 
 //mostrar formularios:
 //document.getElementById("altaArticulo").addEventListener("click",fMostrarAltaArticulo,false);
-document.getElementById("altaCliente").addEventListener("click",fMostarAltaCliente,false);
+//document.getElementById("altaCliente").addEventListener("click",fMostarAltaCliente,false);
 document.getElementById("altaEmpleado").addEventListener("click",fMostarAltaEmpleado,false);
 document.getElementById("altaTaller").addEventListener("click",fMostrarAltaTaller,false);
 document.getElementById("fMostrarBajaModificarCliente").addEventListener("click", fMostrarBajaModificarCliente, false);
@@ -38,7 +38,7 @@ document.getElementById("mostrarCarrito").addEventListener("click",fMostrarCarri
 
 //eventos - botones Dar de ALTA:
 document.getElementById("aceptarAltaEmpleado").addEventListener("click",fAltaEmpleado,false);
-document.getElementById("aceptarAltaCliente").addEventListener("click",aceptarAltaCliente,false);
+//document.getElementById("aceptarAltaCliente").addEventListener("click",aceptarAltaCliente,false);
 //document.getElementById("aceptarAltaArticulo").addEventListener("click",altaArticulo,false);
 document.getElementById("aceptarAltaTaller").addEventListener("click",altaTaller,false);
 
@@ -142,8 +142,16 @@ function loadXMLDoc(filename) {
 function fOcultarFormularios(){
     document.getElementById("divIzquierdo").style.display = "none";
     frmAltaEmpleado.style.display = "none";
-    //frmAltaArticulo.style.display = "none";
-    frmAltaCliente.style.display = "none";
+    if(document.getElementById("frmAltaArticulo") != null){
+        frmAltaArticulo.style.display = "none";
+    }
+    /*if($.contains(document.body, document.getElementById("frmAltaArticulo") )) {
+        frmAltaArticulo.style.display = "none";
+    }*/
+    if(document.getElementById("frmAltaCliente") != null){
+        frmAltaCliente.style.display = "none";
+    }
+    //frmAltaCliente.style.display = "none";
     frmAltaTaller.style.display = "none";
     frmModCliente.style.display = "none";
     frmAltaReparacion.style.display = "none";
@@ -539,80 +547,7 @@ function fMostrarListadoPeriodoComprados(){
     
     
 //fin alta empleado
-//Alta cliente
-    function aceptarAltaCliente(){
-        let dni = frmAltaCliente.dni.value.trim();
-        let nombre = frmAltaCliente.nombre.value.trim();
-        let apellidos = frmAltaCliente.apellidos.value.trim();
-        let direccion = frmAltaCliente.direccion.value.trim();
-        let correo = frmAltaCliente.correo.value.trim();
-        let tlf = frmAltaCliente.tlf.value.trim();
-        let mensaje = "ERROR:";
-        let bValido = true;
-        limpiarErrores();
-        if(/^(X(-|\.)?0?\d{7}(-|\.)?[A-Z]|[A-Z](-|\.)?\d{7}(-|\.)? [0-9A-Z]|\d{8}(-|\.)?[A-Z])$/.test(dni) == false){
-            bValido = false;
-            mensaje += "\nEl DNI es incorrecto";
-            frmAltaCliente.dni.classList.add("error");
-            frmAltaCliente.dni.focus();
-        }
-        if(/[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,48}/.test(nombre) == false){
-            if(bValido){
-                bValido = false;
-                frmAltaCliente.nombre.focus();
-            }
-            frmAltaCliente.nombre.classList.add("error");
-            mensaje += "\nEl nombre es incorrecto";
-        }
-        if(/[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,64}/.test(apellidos) == false){
-            if(bValido){
-                bValido = false;
-                frmAltaCliente.apellidos.focus();
-            }
-            mensaje += "\nLos apellidos son incorrectos";
-            frmAltaCliente.apellidos.classList.add("error");
-        }
-        if(/^[a-zA-Z\s0-9]{2,20}$/.test(direccion) == false){
-            if(bValido){
-                bValido = false;
-                frmAltaCliente.direccion.focus();
-            }
-            mensaje += "\nLa dirección es incorrecta";
-            frmAltaCliente.direccion.classList.add("error");
-        }
-        if(/[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/.test(correo) == false){
-            if(bValido){
-                bValido = false;
-                frmAltaCliente.correo.focus();
-            }
-            mensaje += "\nEl correo es incorrecto";
-            frmAltaCliente.correo.classList.add("error");
-        }
-        if(/^[9678]\d{8}$/.test(tlf) == false){
-            if(bValido){
-                bValido = false;
-                frmAltaCliente.tlf.focus();
-            }
-            mensaje += "\nEl teléfono es incorrecto";
-            frmAltaCliente.tlf.classList.add("error");
-        }
-        if(!bValido){
-            alert(mensaje);
-        }else{
-            alert(oUpoBebe.altaCliente(new Cliente(dni, nombre, apellidos, direccion, correo, tlf)));
-            frmAltaCliente.reset();
-        }
 
-        function limpiarErrores(){
-            frmAltaCliente.dni.classList.remove("error");
-            frmAltaCliente.nombre.classList.remove("error");
-            frmAltaCliente.apellidos.classList.remove("error");
-            frmAltaCliente.direccion.classList.remove("error");
-            frmAltaCliente.correo.classList.remove("error");
-            frmAltaCliente.tlf.classList.remove("error");
-        }
-    }
-//Fin alta cliente
 
 
 //Alta artículo
