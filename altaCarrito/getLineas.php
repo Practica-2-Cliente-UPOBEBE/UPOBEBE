7bba28895b45614d896f991f8cc3e24ac8473abe
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-19 20:19:04 +0100

    Update getLineas.php

---
+++ altaCarrito/getLineas.php
@@ -0,0 +1,34 @@
+<?php
+
+// Configuración BASE DE DATOS MYSQL
+$servidor  = "localhost";
+$basedatos = "upobebe";
+$usuario   = "root";
+$password  = "";
+
+
+
+// Creamos la conexión al servidor.
+$conexion = mysqli_connect($servidor, $usuario, $password,$basedatos) or die(mysqli_error($conexion));
+mysqli_set_charset($conexion,"utf8");
+
+// Consulta SQL para obtener los datos de los centros.
+$sql = "select * from lineas_ventas where id_venta is null";
+$resultados = mysqli_query($conexion,$sql) or die(mysqli_error($conexion));
+
+while($fila = mysqli_fetch_assoc($resultados)){
+    $arrayDatos[] = $fila;
+}
+
+
+if (count($arrayDatos) == 0){
+    $respuesta["existe"] = 0;
+} else {
+    $respuesta["existe"] = 1;
+}
+$respuesta["datos"] = $arrayDatos;
+
+echo json_encode($respuesta);
+
+mysqli_close($conexion);
+?>
