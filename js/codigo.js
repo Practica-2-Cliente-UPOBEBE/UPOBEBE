09c057e2ed43012a5a831b430caa87d2411cad12
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-19 11:28:48 +0100

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -34,7 +34,7 @@ document.getElementById("mostrarListadoArticulo").addEventListener("click",fMost
 document.getElementById("mostrarFormularioVentasPeriodo").addEventListener("click",fMostrarFormularioVentasPeriodos,false);
 document.getElementById("aceptarListarVentasPeriodo").addEventListener("click",fMostrarListadoVentasPeriodo,false);
 //evento - mostrar carrito:
-document.getElementById("mostrarCarrito").addEventListener("click",fMostrarCarrito,false);
+//document.getElementById("mostrarCarrito").addEventListener("click",fMostrarCarrito,false);
 
 //eventos - botones Dar de ALTA:
 document.getElementById("aceptarAltaEmpleado").addEventListener("click",fAltaEmpleado,false);
@@ -671,75 +671,7 @@ function altaTaller() {
 //Fin alta taller
 
 //** fin ALTAS */
-///////////////////////// Mostrar carrito /////////////////////////
 
-function fMostrarCarrito(){
-
-    //Si hay alguna linea que aun no se le ha asignado a una venta es porque esta en el carrito, entonces se pinta en pantalla:
-    if(oUpoBebe.tLineaArticulo.length == 0 || oUpoBebe.tLineaArticulo[(oUpoBebe.tLineaArticulo.length-1)].oVenta != null){
-        alert("Carrito vacío");
-    }else{
-        fOcultarFormularios();
-        fOcultarTablasListado();
-        contadorTotalLineas = 0;
-        let tablaLineas = document.createElement("TABLE");
-        tablaLineas.className = "table table-striped table-hover";
-        tablaLineas.id = "tablaCarrito";
-        let cabecera = tablaLineas.createTHead();
-        let filaCabecera = cabecera.insertRow(-1);
-        filaCabecera.insertCell(-1).textContent = "ARTÍCULO";
-        filaCabecera.insertCell(-1).textContent = "UNIDADES";
-        filaCabecera.insertCell(-1).textContent = "PRECIO";
-        filaCabecera.insertCell(-1).textContent = "TOTAL DE LÍNEA";
-        let cuerpoTabla = tablaLineas.createTBody();
-        oUpoBebe.tLineaArticulo.forEach(elemento =>{
-            if(elemento.oVenta == null){
-                let botonMasUno = document.createElement("INPUT");
-                botonMasUno.setAttribute("type", "button");
-                botonMasUno.setAttribute("value", "+");
-                botonMasUno.addEventListener("click", añadirUnProducto, false);
-                let botonMenosUno = document.createElement("INPUT");
-                botonMenosUno.setAttribute("type", "button");
-                botonMenosUno.setAttribute("value", "-");
-                botonMenosUno.addEventListener("click", quitarUnProducto, false);
-                let botonEliminar = document.createElement("INPUT");
-                botonEliminar.setAttribute("type", "button");
-                botonEliminar.setAttribute("value", "X");
-                botonEliminar.addEventListener("click", eliminarProducto, false);
-
-                let td = document.createElement("TD");
-                td.appendChild(botonMasUno);
-                td.appendChild(botonMenosUno);
-                td.appendChild(botonEliminar);
-
-                let fila = cuerpoTabla.insertRow(-1);
-                fila.insertCell(-1).textContent = elemento.oArt.nombreArticulo;
-                fila.insertCell(-1).textContent = elemento.unidades;
-                fila.insertCell(-1).textContent = elemento.oArt.precioArticulo;
-                fila.insertCell(-1).textContent = elemento.totalLinea();
-                fila.appendChild(td);
-                contadorTotalLineas += elemento.totalLinea();
-            }
-            
-            
-        });
-        let fila = cuerpoTabla.insertRow(-1);
-        let celda = fila.insertCell(-1);
-        celda.textContent ="TOTAL PEDIDO";
-        celda.setAttribute("rowspan", "3");
-        fila.insertCell(-1).textContent = contadorTotalLineas;
-        //Borro la tabla antigua y meto esta
-        document.getElementById("body").removeChild(document.getElementById("body").lastChild);
-        document.getElementById("body").appendChild(tablaLineas);
-        //Muestro el formulario para insertar los dni de cliente y empleado
-        let formu = document.getElementById("formularioCompra");
-        if(formu){
-            formu.style.visibility = "visible";
-        }
-        
-        document.getElementById("aceptarCompra").addEventListener("click", darAltaCompra, false);
-    }
-}
 //Pulsar el botón de comprar
 function darAltaCompra(){
     //Recoger en variables los dni de cliente y vendedor
