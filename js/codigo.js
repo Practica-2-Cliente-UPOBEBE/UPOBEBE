70fecb3cd528f3beecc73f2b7f503d7f2c5de19e
Fredi9889 <alfredolopeznogueroles@gmail.com>
2020-02-20 01:27:34 +0100

    Update codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -676,46 +676,7 @@ function altaTaller() {
 
 //** fin ALTAS */
 
-//Pulsar el botón de comprar
-function darAltaCompra(){
-    //Recoger en variables los dni de cliente y vendedor
-    let dniCliente = document.getElementById("dniCliente").value.trim();
-    let dniEmpleado = document.getElementById("dniEmpleado").value.trim();
-    //Comprobar que existan
-    
-    let oCliente = oUpoBebe.buscarCliente(dniCliente);
-    let oEmpleado = oUpoBebe._buscarEmpleado(dniEmpleado);
-    if(oCliente == null){
-        alert("No hay ningún cliente con ese DNI");
-        document.getElementById("dniCliente").classList.add("error");
-        document.getElementById("dniCliente").focus();
-    }else if(oEmpleado == null){
-        alert("No hay ningún empleado con ese DNI");
-        document.getElementById("dniEmpleado").classList.add("error");
-        document.getElementById("dniEmpleado").focus();
-        document.getElementById("dniCliente").classList.remove("error");
-    }else{
-        let arrayLineas = oUpoBebe.tLineaArticulo.filter(linea => linea.oVenta == null);
-        let f = new Date();
-        let fecha = f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
-        let nuevaVenta = oUpoBebe.altaVenta(new Venta(idVenta, oCliente, oEmpleado, arrayLineas, fecha));
-        if(nuevaVenta){
-            //Añadir el objeto venta a las lineas correspondientes
-            arrayLineas.forEach(linea => {
-                linea.oVenta = nuevaVenta;
-            });
 
-            idVenta++;
-            alert("Venta realizada con éxito");
-            document.getElementById("dniCliente").classList.remove("error");
-            document.getElementById("dniEmpleado").classList.remove("error");
-            document.getElementById("formularioCompra").reset();
-            
-        }else{
-            alert("Error en la venta");
-        }
-    }
-}
 //Boton +
 function añadirUnProducto(oEvento){
     let oE = oEvento || window.event;
